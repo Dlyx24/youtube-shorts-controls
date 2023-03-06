e954dd276c94dbedad6dd39e8145c69d047990ac
Hugo Alexandre <hugo_alexandre@outlook.pt>
2023-03-06 17:55:51 +0000

    Fixes script not loading when navigating to shorts

--- firefox-extension/index.js
+++ firefox-extension/index.js
@@ -1,21 +1,28 @@
 const videoId = "shorts-controls";
 
+let locationInterval;
 const addControls = () => {
   "use strict";
+
+  window.clearInterval(locationInterval);
 
   const thumbnail = document.getElementsByClassName(
     "ytp-cued-thumbnail-overlay"
   );
 
-  if (thumbnail[0] && thumbnail[0].style.display !== "none") {
+  if (
+    thumbnail[0] &&
+    (!thumbnail[0].style.display === "none" || !thumbnail[0].style === "")
+  ) {
     // Video not started, re-runs the function until it has.
     return setTimeout(() => addControls(), 500);
   }
 
-  const video = document.getElementsByClassName(
-    "video-stream html5-main-video"
-  );
-  const myVideo = video[0];
+  const video = document.getElementsByTagName("video");
+  // Starting on the shorts page only adds 1 video element.
+  // Starting on youtube and navigate to shorts page adds 2 video elements and uses the 2nd one.
+  // Don't ask me, ask youtube
+  const myVideo = video[1] ?? video[0];
 
   if (!myVideo) {
     // for any reason video wasn't loaded yet, we re-run the script.
@@ -61,18 +68,19 @@ const addControls = () => {
     const playerControls = document.getElementsByClassName("player-controls");
 
     myVideo.setAttribute("controls", "true");
+
     myVideo.style.cursor = "pointer";
+    myVideo.style.left = "0";
+    myVideo.style.width = "100%";
 
     // Removes most of youtube divs that are full of crap.
     if (infoPanel) infoPanel.remove();
     if (playerControls.length > 0) playerControls[0].remove();
   }
-
   if (myVideo.getAttribute("id") === videoId) {
     // video has id property set so we don't need to do anything else.
     return;
   }
-
   myVideo.addEventListener("loadedmetadata", () => {
     // video src attribute was changes, this means the video changed and we need to re-run the script.
     setTimeout(() => addControls(), 150);
@@ -87,4 +95,18 @@ const addControls = () => {
   });
 };
 
-addControls();
+var lastHref = document.location.href;
+if (lastHref.includes("/shorts/")) {
+  addControls();
+  return;
+} else {
+  locationInterval = window.setInterval(function () {
+    if (document.location.href !== lastHref) {
+      lastHref = document.location.href;
+
+      if (lastHref.includes("/shorts/")) {
+        addControls();
+      }
+    }
+  }, 800);
+}
--- tampermonkey/index.js
+++ tampermonkey/index.js
@@ -1,32 +1,39 @@
 // ==UserScript==
 // @name         Youtube shorts controls
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.0.1
 // @description  Removes youtube shorts overlay and adds controls so you can go back or forward on the video.
 // @author       Dlyx24
-// @match        https://www.youtube.com/shorts/*
+// @match        https://*.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
 
 const videoId = "shorts-controls";
 
+let locationInterval;
 const addControls = () => {
   "use strict";
+
+  window.clearInterval(locationInterval);
 
   const thumbnail = document.getElementsByClassName(
     "ytp-cued-thumbnail-overlay"
   );
 
-  if (thumbnail[0] && thumbnail[0].style.display !== "none") {
+  if (
+    thumbnail[0] &&
+    (!thumbnail[0].style.display === "none" || !thumbnail[0].style === "")
+  ) {
     // Video not started, re-runs the function until it has.
     return setTimeout(() => addControls(), 500);
   }
 
-  const video = document.getElementsByClassName(
-    "video-stream html5-main-video"
-  );
-  const myVideo = video[0];
+  const video = document.getElementsByTagName("video");
+  // Starting on the shorts page only adds 1 video element.
+  // Starting on youtube and navigate to shorts page adds 2 video elements and uses the 2nd one.
+  // Don't ask me, ask youtube
+  const myVideo = video[1] ?? video[0];
 
   if (!myVideo) {
     // for any reason video wasn't loaded yet, we re-run the script.
@@ -73,17 +80,17 @@ const addControls = () => {
 
     myVideo.setAttribute("controls", "true");
     myVideo.style.cursor = "pointer";
+    myVideo.style.left = "0";
+    myVideo.style.width = "100%";
 
     // Removes most of youtube divs that are full of crap.
     if (infoPanel) infoPanel.remove();
     if (playerControls.length > 0) playerControls[0].remove();
   }
-
   if (myVideo.getAttribute("id") === videoId) {
     // video has id property set so we don't need to do anything else.
     return;
   }
-
   myVideo.addEventListener("loadedmetadata", () => {
     // video src attribute was changes, this means the video changed and we need to re-run the script.
     setTimeout(() => addControls(), 150);
@@ -98,4 +105,18 @@ const addControls = () => {
   });
 };
 
-addControls();
+var lastHref = document.location.href;
+if (lastHref.includes("/shorts/")) {
+  addControls();
+  return;
+} else {
+  locationInterval = window.setInterval(function () {
+    if (document.location.href !== lastHref) {
+      lastHref = document.location.href;
+
+      if (lastHref.includes("/shorts/")) {
+        addControls();
+      }
+    }
+  }, 800);
+}
